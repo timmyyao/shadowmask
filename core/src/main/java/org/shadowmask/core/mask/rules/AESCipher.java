d51b8ce77c52484a62dce964660893e4e2e598e0
root <root@tim.sh.intel.com>
2016-09-27 14:23:11 +0800

    add license and fix some bugs

--- core/src/main/java/org/shadowmask/core/mask/rules/AESCipher.java
+++ core/src/main/java/org/shadowmask/core/mask/rules/AESCipher.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.shadowmask.core.mask.rules;
 
 import java.io.UnsupportedEncodingException;
@@ -53,7 +71,7 @@ public class AESCipher extends CipherEngine {
       // validate key and geberate the secret key
       try {
         skey_spec_ = new SecretKeySpec(key.toString().getBytes("UTF-8"), this.method);
-      }catch(UnsupportedEncodingException e) {
+      } catch(UnsupportedEncodingException e) {
         e.printStackTrace();
       }
 
@@ -82,24 +100,8 @@ public class AESCipher extends CipherEngine {
 
         result = Base64.getEncoder().encodeToString(aesCipher.doFinal(content));
         return result;
-      } catch(NoSuchAlgorithmException e) {
-        e.printStackTrace();
-        return null;
-      } catch(InvalidKeyException e) {
-        e.printStackTrace();
-        return null;
-      } catch(InvalidAlgorithmParameterException e) {
-        e.printStackTrace();
-        return null;
-      } catch(NoSuchPaddingException e) {
-        e.printStackTrace();
-        return null;
-      } catch(BadPaddingException e) {
-        e.printStackTrace();
-        return null;
-      } catch(IllegalBlockSizeException e) {
-        e.printStackTrace();
-        return null;
+      } catch (Exception e) {
+        throw new RuntimeException("There is exception in encrypt!");
       }
     }
 
